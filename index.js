8a1eb660740e7be9b67bc7682cde7b1552f2e6b0
Farkhadov95 <farkhadov950405@gmail.com>
2024-02-19 22:49:04 +0500

    edited cors

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const data = require('./data');
 const express = require('express');
 const app = express();
 
-app.use(cors(
-    {
-        origin: '*',
-    }
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
